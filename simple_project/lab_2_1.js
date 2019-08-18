0bbcb49f4e77ca46350ccb744c0f0482eebd1264
qkrwltn8976 <qkrwltn8976@naver.com>
2019-08-18 18:47:58 +0900

    added comments at lab2_1

--- simple_project/lab_2_1.js
+++ simple_project/lab_2_1.js
@@ -8,7 +8,11 @@ const server = http.createServer((req, res)=>{
         return;
 
     const queryParsed = querystring.parse(url.parse(req.url).query);
+    // 서버에 들어오는 url을 파싱하여 query로 만들고 이를 JSON 객체로 변환
+    // parse() : query 문자열을 JSON 객체로 반환해 리턴
 
+    // 서버에 접속 시 str값을 hashing
+    // crypto 모듈 : 문자열 암호화, 복호화, 해싱 / 다양한 방식의 암호화 도와줌
     crypto.randomBytes(32, (err, buf)=>{
         if(err) {
             res.end(JSON.stringify({msg:err}));
@@ -18,6 +22,7 @@ const server = http.createServer((req, res)=>{
                     res.end(JSON.stringify({msg: 'failed'}));
                 else   
                     res.end(JSON.stringify({msg: 'success', hashed: result.toString('base64')}));
+                    // hashing이 성공하면 JSON 데이터로 Response
             });
         }
     });
@@ -26,6 +31,3 @@ const server = http.createServer((req, res)=>{
 });
 
 
-// URL 주소 입력 형식
-// http://127.0.0.1:3000/?str=jisu
-// 서버 실행 결과 {"msg":"success","hashed":"Abrq06V7RrTk9N6t6d0dT1EATL5Gvlreh6iJIT4HzSo="}
