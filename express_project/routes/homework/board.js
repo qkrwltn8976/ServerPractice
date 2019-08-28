c5362a74c01fd3c5ff02bc4f2cd84f338f9648b6
qkrwltn8976 <qkrwltn8976@naver.com>
2019-08-28 17:00:54 +0900

    mysql-nodejs-docker

--- express_project/routes/homework/board.js
+++ express_project/routes/homework/board.js
@@ -10,27 +10,7 @@ const statusCode = require('../../module/utils/statusCode');
 const resMessage = require('../../module/utils/responseMessage');
 
 router.get('/', async(req, res) => {
-    csvtojson().fromFile('board.csv').then((jsonObj)=>{
-        return new Promise((resolve, reject)=>{
-            if(jsonObj != null)
-                resolve(jsonObj);
-            else 
-            res.status(200).send(util.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.READ_FAIL));
-        }).them((boardData) => {
-            for(var i=0; i<boardData.length; i++) {
-                if(boardData[i].id == req.params.id) 
-                    break;
-            }
-
-            if(i<boardData.length) {
-                delete boardData[i].pw;
-                delete boardData[i].salt;
-                res.status(200).send(util.successTrue(statusCode.OK, resMessage.READ_FAIL, boardData[i]));
-            } else {
-                res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
-            }
-        });
-    });
+    console.log("*************")
 });
 
 
